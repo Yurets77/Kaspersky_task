ac0523027cc881f8558f6dd6ad2ab3d4840b1f46
Yuriy Pchelnikov <pchelnikov.2006@mail.ru>
2025-05-06 12:07:52 +0300

    fix showing tags

--- src/components/mainContent/CounterWords.tsx
+++ src/components/mainContent/CounterWords.tsx
@@ -1,20 +1,20 @@
 export default function CounterWords(text: string): Record<string, number> {
-        const keywords = ["kaspersky", "new", "antivirus"];
-        const counts: Record<string, number> = {
-          kaspersky: 0,
-          new: 0,
-          antivirus: 0,
-        };
-      
-        const regex = /<kw>(.*?)<\/kw>/g;
-        let match;
-      
-        while ((match = regex.exec(text)) !== null) {
-          const word = match[1].toLowerCase();
-          if (keywords.includes(word)) {
-            counts[word] += 1;
-          }
-        }
-      
-        return counts;
+  const keywords = ["kaspersky", "new", "antivirus"];
+  const counts: Record<string, number> = {
+    kaspersky: 0,
+    new: 0,
+    antivirus: 0,
+  };
+
+  const regex = /<kw>(.*?)<\/kw>/g;
+  let match;
+
+  while ((match = regex.exec(text)) !== null) {
+    const word = match[1].toLowerCase();
+    if (keywords.includes(word)) {
+      counts[word] += 1;
+    }
+  }
+
+  return counts;
 }
